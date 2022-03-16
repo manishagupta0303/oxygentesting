9eb629e386b6ad165de796d75650a82d5fdaafc1
manishagupta0303 <manisha.gupta@delphix.com>
2022-03-16 11:51:41 +0530

    dummy

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,15 @@
+define(function() {var keywords=[{w:"Checklist",p:["p0"]},{w:"of",p:["p0"]},{w:"Information",p:["p0"]},{w:"Required",p:["p0"]},{w:"for",p:["p0"]},{w:"Installation",p:["p0"]},{w:"and",p:["p0","p7"]},{w:"Configuration",p:["p0","p1"]},{w:"Context",p:["p2"]},{w:"Sensitive",p:["p2"]},{w:"Help",p:["p2"]},{w:"Deployment",p:["p3","p6"]},{w:"Network",p:["p4"]},{w:"Connectivity",p:["p4"]},{w:"Requirements",p:["p4"]},{w:"Registration",p:["p5"]},{w:"Management",p:["p5"]},{w:"Standard",p:["p6"]},{w:"Architecture",p:["p6"]},{w:"Users",p:["p7"]},{w:"Groups",p:["p7"]}];
+var ph={};
+ph["p0"]=[0, 1, 2, 3, 4, 5, 6, 7];
+ph["p1"]=[7];
+ph["p2"]=[8, 9, 10];
+ph["p3"]=[11];
+ph["p4"]=[12, 13, 14];
+ph["p5"]=[15, 16];
+ph["p6"]=[17, 11, 18];
+ph["p7"]=[19, 6, 20];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
---
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {10:9,12:9,9:-1,11:8,13:8,8:-1,7:9};
+});
